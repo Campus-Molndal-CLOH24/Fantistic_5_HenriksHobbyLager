7a43f811984d888c380767e470711ca46e17c264
Luunomx <91hughem@gafe.molndal.se>
2024-11-29 10:14:58 +0100

    docs: we did it boize

--- HenriksHobbyLager/Program.cs
+++ HenriksHobbyLager/Program.cs
@@ -1,23 +1,4 @@
-﻿/*
-/*
-    HENRIKS HOBBYLAGER™ 1.0
-    Skapat av: Henrik Hobbykodare
-    Datum: En sen kväll i oktober efter fyra Red Bull
-    Version: 1.0 (eller kanske 1.1, jag har ändrat lite sen första versionen)
-
-    TODO-lista:
-    * Lägga till stöd för bilder på produkterna (kanske)
-    * Göra backups (förlorade nästan allt förra veckan när skärmsläckaren startade)
-    * Kolla upp det där med "molnet" som alla pratar om
-    * Lägg till ljudeffekter när man lägger till produkter???
-    * Fixa så att programmet startar automatiskt när datorn startar om
-    * Be någon förklara vad "dependency injection" betyder
-    * Köpa en UPS (strömavbrott är INTE kul!)
-    * Lära mig vad XML är (folk säger att det är viktigt)
-    * Göra en logga till programmet i Paint
-*/
-
-using HenriksHobbyLager.Facades;
+﻿using HenriksHobbyLager.Facades;
 using HenriksHobbyLager.Factories;
 using HenriksHobbyLager.Interfaces;
 using HenriksHobbyLager.Models;
